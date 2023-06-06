d9ce4164ee511ba2ae0ad771a18998a787e07c47
spidyynaik <128451864+spidyynaik@users.noreply.github.com>
2023-06-06 20:29:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -228,6 +228,7 @@ async def advantage_spoll_choker(bot, query):
 
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
+    data = query.data
     if query.data == "close_data":
         await query.message.delete()
     elif query.data == "delallconfirm":
